11e2c25f2b087b6b23ec4a948a8ef166b498b6ac
aditya-d1315 <aditya.dhawan@rutgers.edu>
2024-10-17 01:59:32 -0400

    Fixing whitespace.

--- rot_cubes.js
+++ rot_cubes.js
@@ -8,7 +8,7 @@ var delay = 200;
 
 var numCubeVertices = 36;
 
-var cube_verts  = []; 
+var cube_verts = []; 
 var cube_vert_cols = [];
 var cube_element_indices = [];
 
@@ -18,23 +18,23 @@ var vColor, vPosition;
 var M_comp_loc;
 var theta = 0., phi = 0.;
 
-var M_comp = mat4(); 
+var M_comp = mat4();
 
 // all initializations
 window.onload = function init() {
 	// get canvas handle
-	canvas = document.getElementById( "gl-canvas" );
+	canvas = document.getElementById("gl-canvas");
 
 	// WebGL Initialization
 	gl = initWebGL(canvas);
-	if (!gl ) {
-		alert( "WebGL isn't available" );
+	if (!gl) {
+		alert("WebGL isn't available");
 	}
 
 	// set up viewport
-	gl.viewport( 0, 0, canvas.width, canvas.height );
-	gl.clearColor( 0.8, 0.8, 0.0, 1.0 );
-	gl.clear( gl.COLOR_BUFFER_BIT );
+	gl.viewport(0, 0, canvas.width, canvas.height);
+	gl.clearColor(0.8, 0.8, 0.0, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
 
 	// create shaders, compile and link program
 	program = initShaders(gl, "vertex-shader", "fragment-shader");
@@ -55,15 +55,15 @@ window.onload = function init() {
     // variables through which shader receives vertex and other attributes
 	vPosition = gl.getAttribLocation(program, "vPosition");
 	gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0 );
-	gl.enableVertexAttribArray( vPosition );	
+	gl.enableVertexAttribArray(vPosition);	
 
 	// similarly for color buffer
-	gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer);
-	gl.bufferData( gl.ARRAY_BUFFER, flatten(cube_vert_cols), gl.STATIC_DRAW);
+	gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(cube_vert_cols), gl.STATIC_DRAW);
 
 	vColor = gl.getAttribLocation(program, "vColor");
 	gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0 );
-	gl.enableVertexAttribArray(vColor );
+	gl.enableVertexAttribArray(vColor);
 
 	// index buffer 
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
@@ -84,7 +84,6 @@ function render(){
 
 	// center cube
 	M_comp = matMult(rotate4x4(-10., 'x'), matMult(rotate4x4(10., 'y'), scale4x4(0.1, 0.1, 0.1)));
-
 	gl.uniformMatrix4fv(M_comp_loc, false, flatten(M_comp));
 	gl.drawElements(gl.TRIANGLES, numCubeVertices, gl.UNSIGNED_BYTE, 0);
 
@@ -155,4 +154,3 @@ function getCubeVertexColors() {
 
 	return cols;
 }
-
